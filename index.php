ebf3348358b84c0fda05f2579ec5db54d214ec58
Yuto Shinohara <yutonx@gmail.com>
2017-09-01 10:56:36 +0700

    first commit

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 ﻿<?php require 'header.php'; ?>
-<p>Input Your Name</p>
-<form action="indexA.php" method="post">
-<input type="text" name="user" size="25">
 
 </form>
-
 
 <form action="#" >
 PASS:<input type="password" name="pw" maxlength="5" />
 </form>
 
-<p>Choose place</p>
+<p>Input Your Name</p>
 <form action="indexA.php" method="post">
+<input type="text" name="user" size="25">
+<input type="submit" value="GO!!">
+<br>
+<p>Choose place</p>
+<br>
 <select name="code">
 <option value="100">Tokyo</option>
 <option value="101">Bangkok</option>
