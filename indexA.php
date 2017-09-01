20a5a22c0adc1154658bb60632298afe734d5f91
Yuto Shinohara <yutonx@gmail.com>
2017-09-01 17:39:09 +0700

    first commit

--- indexA.php
+++ indexA.php
@@ -13,5 +13,16 @@ echo 'Hello ,' , $_REQUEST['user'];
 echo 'These Inteternational Access number are ', $_REQUEST['code1'],  ' and ' , $_REQUEST['code2'] ;
 ?>
 
+<?php
+$file='board.txt';
+if (file_exists($file)) {
+	$board=json_decode(file_get_contents($file));
+}
+$board[]=$_REQUEST['user'];
+file_put_contents($file, json_encode($board));
+foreach ($board as $user) {
+	echo '<p>', $user, '</p><hr>';
+}
+?>
 
 <?php require 'footer.php'; ?>
