e5febbce21df4daf04046f29a703cb1dba4e6e67
Yuto Shinohara <yutonx@gmail.com>
2017-09-05 14:52:35 +0700

    first commit

--- index.php
+++ index.php
@@ -35,29 +35,6 @@ foreach ($store as $key=>$value) {
 <p><input type="submit" value="GO!!"></p>
 </form>
 
-<?php
-	$rawData = strtoupper($_GET['d']);
-	$uid = substr($rawData, 0, 14);
-	$flagTamper = substr($rawData, 14, 2);
-	$timeStampTag = (double)hexdec(substr($rawData, 16, 8));
-
-	$rollingCodeTag = substr($rawData, 24, 8);
-	require_once "keystream.php";
-	$rollingCodeServer = keystream(hexbit($rawRowData["key"]), hexbit(substr($rawData, 16, 8)), 4);
-
-	if((strlen($rawRowData["key"]) == 20) && ($rollingCodeServer === $rollingCodeTag)){
-		$Judge='correct';
-	}else{
-		$Judge='incorrect';}
-
-
-	echo "UID is ${uid}"; <br>	
-	echo "TamperStatus is ${flagTamper}";<br>
-	echo "TimeStamp is ${timeStampTag}";<br>
-	echo "RollingCode is ${rollingCodeTag}";<br>
-	echo "This RolligCode is ${Judge}";<br>
-	
-?>
 
 
 <?php require 'footer.php'; ?>
