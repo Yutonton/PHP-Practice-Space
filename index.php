c3f6640ee89c5a68d62b4da3174d1f7989541964
Yuto Shinohara <yutonx@gmail.com>
2017-09-07 17:13:53 +0700

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,53 @@
 ﻿<?php require 'header.php'; ?>
 
-</form>
+
+   
+
+	<div id="fb-root"></div>
+		<script>(function(d, s, id) {
+  			var js, fjs = d.getElementsByTagName(s)[0];
+  			if (d.getElementById(id)) return;
+  			js = d.createElement(s); js.id = id;
+		  	js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.10&appId=280487102433701";
+            fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+		</script>
+	   <!--javascript SDK-->
+
+      <?php
+
+       		 require_once '/vendor/autoload.php';
+        	 $fb = new Facebook\Facebook([
+        	'app_id' => '280487102433701',
+        	'app_secret' => 'a35095e11b54fa8a131234feeb6852c0',
+       		 'default_graph_version' => 'v2.10',
+        	]);
+        	
+
+        	$authenUser = $_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME'];
+        	$headers = getallheaders();
+        	$accessToken = $headers['X-Ms-Token-Facebook-Access-Token'];   
+
+        	try {
+        		$response = $fb->get('/me?fields=id,name,picture', $accessToken);
+        	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+        		echo 'Graph returned an error: ' . $e->getMessage();
+        		exit;
+        	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+        		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        		exit;
+        	}
+
+        	$user = $response->getGraphUser();
+
+        	echo 'FaceBookName  is  : ' . $user['name'] ;
+        	$profile_picture = $user['picture'];
+        	echo '<img src="' . $profile_picture['url'] . '" alt="Profile Image" style="width:240px;height:240px;">';     
+        
+      ?>
+  
+
+
 
 
 <form action="indexA.php" method="post">
@@ -69,6 +116,10 @@ foreach ($store as $key=>$value) {
 	echo "This RolligCode is " .$judge ;
 	
 ?>
+
+	<div class="fb-share-button" data-href="https://internsilicon01.azurewebsites.net/" data-layout="box_count" data-size="large" data-mobile-iframe="true">
+		<a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Finternsilicon01.azurewebsites.net%2F&amp;src=sdkpreparse">シェア</a>
+	</div>
 
 
 <?php require 'footer.php'; ?>
