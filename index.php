c6a1d34484ab3c51dfd947ba2e72fbb5e5b2bd46
Yuto Shinohara <yutonx@gmail.com>
2017-09-08 10:21:53 +0700

    first commit

--- index.php
+++ index.php
@@ -15,11 +15,12 @@
 <?php
 
        		 require_once '/vendor/autoload.php';
-        	 $fb = new Facebook\Facebook([
-       /* 	'app_id' => '280487102433701',
-        	'app_secret' => 'a35095e11b54fa8a131234feeb6852c0',
-       		 'default_graph_version' => 'v2.10',*/
-			]);
+
+			$fb = new Facebook\Facebook([
+				'app_id' => '280487102433701',
+				'app_secret' => 'a35095e11b54fa8a131234feeb6852c0',
+				'default_graph_version' => 'v2.10',
+				]);
 			
 			echo "A";
         	$authenUser = $_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME'];
