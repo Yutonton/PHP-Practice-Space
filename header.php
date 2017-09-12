b3d56ae99d0a52a9cce43830ec5417ee9f31aed4
Yuto Shinohara <yutonx@gmail.com>
2017-09-12 17:51:08 +0700

    first commit

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>POCARISWEAT~SUMMERCAMPAIGN~</title>
-<link href="https://fonts.googleapis.com/css?family=Exo:400,900" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Exo:900" rel="stylesheet"> 
 <link rel="stylesheet" href="style1.css">
 </head>
 <body>
