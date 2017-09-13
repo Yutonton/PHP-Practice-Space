764c6aad2da02da90991447aeb469872a4b7473e
Yuto Shinohara <yutonx@gmail.com>
2017-09-13 16:51:29 +0700

    first commit

--- database1.php
+++ database1.php
@@ -122,10 +122,10 @@ function readRowDatabase($uid)
 		// http://msdn.microsoft.com/library/azure/dd179438.aspx
 		$code = $e->getCode();
         $error_message = $e->getMessage();
-        echo "Error code:";// . $code; ."\n";
-        echo "Error msg:";// $error_messagecode "\n";
-	}
 
+	}
+    echo "Error code:";// . $code; ."\n";
+    echo "Error msg:";// $error_messagecode "\n";
 	$entities = $result->getEntities();
 	
 	if(!empty($entities))
