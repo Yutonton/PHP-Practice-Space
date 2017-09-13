d1bac8befe490c9e375600beaa05504940899332
Yuto Shinohara <yutonx@gmail.com>
2017-09-13 16:46:31 +0700

    first commit

--- database1.php
+++ database1.php
@@ -102,8 +102,8 @@ function updateRowDatabase($arrayRawUpdateData)
 		// http://msdn.microsoft.com/library/azure/dd179438.aspx
 		$code = $e->getCode();
         $error_message = $e->getMessage();
-        echo "Error code:". $code; ."\n";
-        echo "Error msg:". $error_messagecode "\n";
+        echo "Error code:"; //. $code; ."\n";
+        echo "Error msg:"; // $error_messagecode "\n";
 	}
 }
 
