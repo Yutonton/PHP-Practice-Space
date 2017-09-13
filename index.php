5c4982d206649d78b78b041a01c21fad29473c74
Yuto Shinohara <yutonx@gmail.com>
2017-09-13 17:40:31 +0700

    first commit

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ PASS(7 numbers):<input type="password" name="pw" maxlength="5" />
 
 	echo $rawRowData["OwnerID"];
 	// if(empty($rawRowData["OwnerID"])){
-	if(!empty($rawRowData["OwnerID"]))
+	if(empty($rawRowData["OwnerID"]))
 	{
 		 $rawRowData["OwnerID"]=$user['id']; 
 		 //echo $rawRowData["OwnerID"];
