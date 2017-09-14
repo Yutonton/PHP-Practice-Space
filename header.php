f435e22bd79996a60eb11195c526d9038d6d29ab
Yuto Shinohara <yutonx@gmail.com>
2017-09-14 09:10:16 +0700

    first commit

--- header.php
+++ header.php
@@ -3,8 +3,11 @@
 <head>
 <meta charset="UTF-8">
 <title>~SUMMERCAMPAIGN~</title>
-<link href="https://fonts.googleapis.com/css?family=Exo:900" rel="stylesheet"> 
 <link rel="stylesheet" href="normalize.css">
+
+<link href="https://fonts.googleapis.com/css?family=Exo:900" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC" rel="stylesheet"> 
+
 <link rel="stylesheet" href="style2.css">
 </head>
 <body>
