3b9c67b3b87d3de32e6863f36294f3363f17dc2c
Yuto Shinohara <yutonx@gmail.com>
2017-09-14 14:09:15 +0700

    first commit

--- header.php
+++ header.php
@@ -3,14 +3,14 @@
 <head>
 <meta charset="UTF-8">
 <title>~SUMMERCAMPAIGN~</title>
-<!--<link rel="stylesheet" href="normalize.css"> -->
+<<link rel="stylesheet" href="normalize.css"> 
 
 <link href="https://fonts.googleapis.com/css?family=Exo:900" rel="stylesheet"> 
 <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC" rel="stylesheet"> 
 
 
-<link rel="stylesheet" type="text/css" href="style2-PC.css">
-<link rel="stylesheet" media="screen and (max-width: 480px)"type="text/css"  href="style2-phone.css">
+<!--<link rel="stylesheet" type="text/css" href="style2-PC.css">-->
+<link rel="stylesheet" media="screen and (max-width: 480px)" type="text/css"  href="style2-phone.css">
 
 </head>
 <body>
