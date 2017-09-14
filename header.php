da7112e8403f7024ff9f4f7adf32657aeb07fcef
Yuto Shinohara <yutonx@gmail.com>
2017-09-14 14:01:37 +0700

    first commit

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
 <link href="https://fonts.googleapis.com/css?family=Bowlby+One+SC" rel="stylesheet"> 
 
 
-<link rel="stylesheet"  href="style2-PC.css">
-<link rel="stylesheet" media="screen and (max-width: 480px)"  href="style2-phone.css">
+<link rel="stylesheet" type="text/css" href="style2-PC.css">
+<link rel="stylesheet" media="screen and (max-width: 480px)"type="text/css"  href="style2-phone.css">
 
 </head>
 <body>
