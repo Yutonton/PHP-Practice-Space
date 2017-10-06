8f2dc10715b314b19615c3bb681d995b9e303a96
Yuto Shinohara <yutonx@gmail.com>
2017-10-06 14:04:29 +0900

    first commit

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>~SUMMERCAMPAIGN~</title>
+<title>YutoShinohara</title>
 <link rel="stylesheet" href="normalize.css"> 
 <meta name="viewport" content="width=device-width,initial-scale=1">
 
